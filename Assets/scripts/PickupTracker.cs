54a9e1151fd33a411e1253169f4cb3e848cda389
smilewood <milewood@gmail.com>
2018-04-22 17:07:12 -0600

    pickups are in and tracked

---
+++ Assets/scripts/PickupTracker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class PickupTracker : MonoBehaviour
+{
+    public int collected, total;
+
+    private void Start()
+    {
+        collected = 0;
+    }
+    public void Pickup()
+    {
+        collected++;
+    }
+}
